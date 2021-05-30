f4a79084df395e6ec5013dd7c6af8c4687b9a421
Kevin Cordoba Chevez <kecordoba@ic-itcr.ac.cr>
2021-05-29 19:53:21 -0600

    Borrador De Controlador
    Se añade un proyecto vacío para la aplicación controlador.

---
+++ Controlador/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Controlador controlador = new Controlador();
+        //System.out.println("Hello, World!");
+    }
+}
---
+++ Controlador/src/Controlador.java
@@ -0,0 +1,40 @@
+import java.net.*;
+import java.io.*;
+
+public class Controlador implements Runnable{
+
+    public boolean running;
+
+    Controlador(){
+        Thread hilo = new Thread(this);
+        hilo.start();
+        running = true;
+    }
+    
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+       /* try {
+            
+            ServerSocket servidor = new ServerSocket(9999);
+
+            while(running){
+                Socket miSocket = servidor.accept();
+                DataInputStream flujoEntrada = new DataInputStream(miSocket.getInputStream());
+                String mensaje = flujoEntrada.readUTF();
+                //areaTexto.append("\n" + mensaje);
+                miSocket.close();
+            }
+            
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } */
+        
+    }
+
+    
+}
