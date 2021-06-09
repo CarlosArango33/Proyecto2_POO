8fdfb9892d5175eb40526fd13ebf8e95037b870b
Carlos Arango <cmarango99@gmail.com>
2021-06-09 15:54:09 -0600

    cambios peque;os

--- src/Vuelos/src/Avion.java
+++ src/Vuelos/src/Avion.java
@@ -1,7 +1,31 @@
 package Vuelos.src;
 
-public class Avion {
+import java.io.IOException;
 
-    
+public class Avion extends Thread{
+
+    private String type;
+    private String size;
+    private Boolean retraso;
+
+    public Avion(String tipo, String tamanno, Boolean estado_retraso) {
+        String type = tipo;
+        String size = tamanno;
+        Boolean retraso = estado_retraso;
+
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getSize(){
+        return size;
+    }
+
+    public Boolean getRetraso(){
+        return retraso;
+    }
+
 
 }
--- src/Vuelos/src/Vuelos.java
+++ src/Vuelos/src/Vuelos.java
@@ -1,7 +1,28 @@
 package Vuelos.src;
 
+import java.util.ArrayList;
+
 public class Vuelos {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         /*
+         La idea es enviar cada cierto tiempo un avion al servidor
+         entonces enviar ya sea en formato String, Json o clase el vuelo con
+         sus caracteristicas para procesarr en controlador
+         */
+        ArrayList lista = new ArrayList();
+
+        Avion AvionPriv = new Avion("privado", "peque", false);
+        Avion AvionPasajeros = new Avion("pasajeros", "mediano", true);
+        Avion AvionCarga = new Avion("carga", "mediano", false);
+
+        lista.add(AvionPriv);
+        lista.add(AvionCarga);
+        lista.add(AvionPasajeros);
+
+
     }
+
+    //Agregar Hilo
+    //Mandar lista al  servidor para que mande los archivos/clases
 }
