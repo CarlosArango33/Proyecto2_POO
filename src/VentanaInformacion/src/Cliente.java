49335ecd3d2db807b0644a869b3c46d69794bf9a
Carlos Arango <cmarango99@gmail.com>
2021-06-10 15:39:14 -0600

    .

---
+++ src/VentanaInformacion/src/Cliente.java
@@ -0,0 +1,11 @@
+package VentanaInformacion.src;
+
+/*
+Esto (si se puede) deberia ser el que envia diferentes mensajes
+la idea es usar este unico cliente y al usarlo en hilos que pueda hacer varias tareas
+
+ */
+public class Cliente extends Thread {
+
+
+}
